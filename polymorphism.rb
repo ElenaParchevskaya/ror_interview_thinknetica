2a99fa6f6668510c88e9979aac70d23df0de6a40
Elena Parchevskaya <elena.lemoon@gmail.com>
2022-03-18 12:41:49 +0100

    refactoring using polymorphism for an example

---
+++ polymorphism.rb
@@ -0,0 +1,30 @@
+class Search
+  def search(type)
+    raise 'Unknown search type' unless type.respond_to?(:search)
+    type.search
+  end
+end
+
+class Job
+  def search
+    search_jobs
+  end
+
+  private
+
+  def search_jobs
+    #searchng
+  end
+end
+
+class User
+  def search
+    search_users
+  end
+
+  private
+
+  def search_users
+    #searchng
+  end
+end
